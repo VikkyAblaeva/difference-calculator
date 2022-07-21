d508c55a531d7a1c48f225b3f601e359be73b518
vika ablaeva <nifergirl@yandex.ru>
2022-07-21 11:55:24 +0300

    fix test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -5,9 +5,9 @@ import { genDiff } from '../src/utils.js';
 
 const pathOfInitialFile = '__fixtures__/file1.json';
 const pathOfChangedFile = '__fixtures__/file2.json';
-const expected = fs.readFileSync(path.resolve('__fixtures__/result.txt'), { encoding: 'utf8', flag: 'r' });
+const expected = fs.readFileSync(path.resolve('__tests__/__fixtures__/result.txt'), { encoding: 'utf8', flag: 'r' });
 const received = genDiff(pathOfInitialFile, pathOfChangedFile);
 
 test('genDiff', () => {
-  expect(received).toStrictEqual(expected);
+  expect(received).toBe(expected);
 });
