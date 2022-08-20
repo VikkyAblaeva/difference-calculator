d899dc4ea47c1db6058a37353ee6a62b05d63eb2
VikkyAblaeva <nifergirl@yandex.ru>
2022-08-20 15:37:31 +0300

    fix main

--- src/main.js
+++ src/main.js
@@ -5,14 +5,17 @@ import yaml from 'js-yaml';
 import getFormat from './formatters/index.js';
 
 const getObjectWithData = (filepath) => {
-  let objectWithData = {};
-  if (filepath.toString().slice(-4) === 'json') {
-    objectWithData = JSON.parse(fs.readFileSync(path.resolve(filepath), { encoding: 'utf8', flag: 'r' }));
+  const extension = filepath.toString().slice(-4);
+  switch (extension) {
+    case 'json':
+      return JSON.parse(fs.readFileSync(path.resolve(filepath), { encoding: 'utf8', flag: 'r' }));
+    case '.yml':
+      return yaml.load(fs.readFileSync(path.resolve(filepath), 'utf8'));
+    case 'yaml':
+      return yaml.load(fs.readFileSync(path.resolve(filepath), 'utf8'));
+    default:
+      return 'Error! Unknown format!';
   }
-  if (filepath.toString().slice(-3) === 'yml' || filepath.toString().slice(-4) === 'yaml') {
-    objectWithData = yaml.load(fs.readFileSync(path.resolve(filepath), 'utf8'));
-  }
-  return objectWithData;
 };
 
 const buildTree = (tree1, tree2) => {
