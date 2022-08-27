263d748a3564ce9ad42b276f1d78a2ccaf0b9963
VikkyAblaeva <nifergirl@yandex.ru>
2022-08-27 19:07:20 +0300

    fix stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,6 +2,8 @@ import _ from 'lodash';
 
 const getIndent = (depth, spacesCount = 4) => `  ${' '.repeat(spacesCount).repeat(depth - 1)}`;
 
+const getLine = (key, value, char) => `${char}${key}: ${value}`;
+
 const getStylishFormat = (body, depth, spacesCount = 4) => `{\n${body}\n${' '.repeat(spacesCount).repeat(depth)}}`;
 
 const getValue = (value, depth) => {
@@ -9,7 +11,7 @@ const getValue = (value, depth) => {
     return value;
   }
   const entries = Object.entries(value);
-  const items = entries.map(([key, val]) => `${getIndent(depth + 1)}  ${key}: ${getValue(val, depth + 1)}`);
+  const items = entries.map(([key, val]) => `${getIndent(depth + 1)}${getLine(key, getValue(val, depth + 1), '  ')}`);
   const body = items.join('\n');
   return getStylishFormat(body, depth);
 };
@@ -19,12 +21,14 @@ const stylish = (diff, depth) => {
     const symbols = { added: '+ ', removed: '- ', unchanged: '  ' };
     switch (status) {
       case 'updated':
-        return [`${getIndent(depth + 1)}${symbols.removed}${key}: ${getValue(value.oldValue, depth + 1)}`,
-          `${getIndent(depth + 1)}${symbols.added}${key}: ${getValue(value.newValue, depth + 1)}`];
+        return [
+          `${getIndent(depth + 1)}${getLine(key, getValue(value.oldValue, depth + 1), symbols.removed)}`,
+          `${getIndent(depth + 1)}${getLine(key, getValue(value.newValue, depth + 1), symbols.added)}`,
+        ];
       case 'nested':
-        return `${getIndent(depth + 1)}  ${key}: ${stylish(value, depth + 1)}`;
+        return `${getIndent(depth + 1)}${getLine(key, stylish(value, depth + 1), '  ')}`;
       default:
-        return `${getIndent(depth + 1)}${symbols[status]}${key}: ${getValue(value, depth + 1)}`;
+        return `${getIndent(depth + 1)}${getLine(key, getValue(value, depth + 1), symbols[status])}`;
     }
   });
   const formattedDiffs = diffLines.join('\n');
